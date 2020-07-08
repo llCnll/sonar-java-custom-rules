1addecd9f0c577a068b0aaa5154132e2af67c35c
陈南 <chennan@aobi.com>
2020-07-08 09:44:19 +0800

    refactor:remove unused header

--- src/test/java/org/sonar/samples/java/checks/MyFirstCustomCheckTest.java
+++ src/test/java/org/sonar/samples/java/checks/MyFirstCustomCheckTest.java
@@ -3,12 +3,6 @@ package org.sonar.samples.java.checks;
 import org.junit.Test;
 import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import java.time.LocalDateTime;
-import java.util.Date;
-
 /**
  * @author cn
  * @date 2020-07-03 16:30
